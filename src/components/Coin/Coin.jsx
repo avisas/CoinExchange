f4ebd692b3ab79366ec4a34013facc972c48558d
alejandravisa <alejandra.visa91@gmail.com>
2021-07-13 06:18:51 -0500

    change price every second

--- src/components/Coin/Coin.jsx
+++ src/components/Coin/Coin.jsx
@@ -3,12 +3,31 @@ import './Coin.css';
 import PropTypes from 'prop-types';
 
 export default class Coin extends Component {
+  constructor(props) {
+    super(props);
+      this.state = {
+        price: this.props.price
+      }
+  }
+
+  // catch state when component mount to DOM
+  componentDidMount() {
+    const callback = () => {
+      const randomPercentage = 0.995 + Math.random() * 0.01;
+      this.setState( function(oldState) {
+        return {
+          price: oldState.price * randomPercentage
+        };
+      });
+    }
+    setInterval( callback, 1000);
+  }
     render() {
         return (
             <tr className='coin-row'>
               <td>{this.props.name}</td>
               <td>{this.props.ticker}</td> 
-              <td>${this.props.price}</td>  
+              <td>${this.state.price}</td>  
             </tr>
           );
     }
