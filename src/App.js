5c50e3e7135a3079dae558a18916cc75092b889c
alejandravisa <alejandra.visa91@gmail.com>
2021-07-21 06:48:58 -0500

    rendering lists

--- src/App.js
+++ src/App.js
@@ -4,36 +4,72 @@ import './App.css';
 import Coin from './components/Coin/Coin';
 import AccountBalance from './components/AccountBalance/AccountBalance';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} alt="React logo" className="App-logo"></img>
-        <h1>
-          Coin Exchange
-        </h1>
-      </header>
-      <AccountBalance amount={10000}/>
-      <table className="coin-table">
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Ticker</th>
-          <th>Price</th>
-        </tr>
-      </thead>
-      <tbody>
-        <Coin name="Bitcoin" ticker = "BTC" price ={9999.99}/>
-        <Coin name="Ethereum" ticker = "ETH" price ={299.99}/>
-        <Coin name="Tether" ticker ="USDT" price={1.0}/>
-        <Coin name="Ripple" ticker="XRP" price={0.2}/>
-      </tbody>
-    </table>
-  </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      balance: 10000,
+      coinData: [
+        {
+          name: 'Bitcoin',
+          ticker: 'BTC',
+          price: 9999.99
+        },
+        {
+          name: 'Ethereum',
+          ticker: 'ETH',
+          price: 299.99
+        },
+        {
+          name: 'Tether',
+          ticker: 'USDT',
+          price: 1.0
+        },
+        {
+          name: 'Ripple',
+          ticker: 'XRP',
+          price: 0.2
+        },
+        {
+          name: 'Bitcoin Cash',
+          ticker: 'BCH',
+          price: 298.99
+        }
+      ]
+    }
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} alt="React logo" className="App-logo"></img>
+          <h1>
+            Coin Exchange
+          </h1>
+        </header>
+        <AccountBalance amount={this.state.balance} />
+        <table className="coin-table">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Ticker</th>
+              <th>Price</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              this.state.coinData.map( value => 
+                <Coin key={value.ticker} {...value}/> 
+              )
+            }
+          </tbody>
+        </table>
+      </div>
+    );
+  }
 }
 
-// to represent a number, fractional number -> put in brackets.
+  // to represent a number, fractional number -> put in brackets.
 
 
 export default App;
