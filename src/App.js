8f966f549c0f3ddfccc13536e8cce8c9e96feec3
alejandravisa <alejandra.visa91@gmail.com>
2021-08-04 06:19:48 -0500

    API communication

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ const Div = styled.div`
   color: antiquewhite;
 `;
 
-const CoinCount = 10;
+const Coin_Count = 10;
 
 class App extends React.Component {
   state = {
@@ -54,18 +54,24 @@ class App extends React.Component {
   }
 
   componentDidMount = async() => {
-    let response = await axios.get('https://api.coinpaprika.com/v1/coins')
-    let coinData = response.data.slice(0, CoinCount).map(function(coin){
+    const response = await axios.get('https://api.coinpaprika.com/v1/coins');
+    const coinIds = response.data.slice(0, Coin_Count).map(coin => coin.id);
+    const tickerUrl = 'https://api.coinpaprika.com/v1/tickers';
+    const promises = coinIds.map(id => axios.get(tickerUrl + id));
+    const coinData = await Promise.all(promises);
+    const coinPriceData = coinData.map(function(response) {
+      const coin = response.data;
       return {
         key: coin.id,
         name: coin.name,
         ticker: coin.symbol,
         balance: 0,
-        price: 0
-      }
-    });
-    this.setState({coinData});
-  }
+        price: parseFloat(Number(coin.quotes.USD.price).toFixed(4)),
+      };
+    })
+    // Retrieve the prices
+    this.setState({coinData: coinPriceData});
+  };
 
   handleBalanceVisibilityChange = () => {
     this.setState( function(oldState) {
